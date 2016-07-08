61e3b67a3792969905cdcb51487fcac4b4835974
aweful <anthony.mcdonald@asu.edu>
2016-07-08 23:15:36 +0000

    start add form routing

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
+import { reducer as form } from 'redux-form';
 
 import game from './game';
 import settings from './settings';
 
 export default combineReducers({
-    game, settings, routing
+    game, settings, routing, form
 });
