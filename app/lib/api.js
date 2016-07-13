2f8721d5365bb54b3bad3e663ca95753534b55f6
aweful <anthony.mcdonald@asu.edu>
2016-07-13 03:37:35 +0000

    add api.js file to project

---
+++ app/lib/api.js
@@ -0,0 +1,31 @@
+import fetch from 'isomorphic-fetch';
+import url from 'url';
+
+function makeUrl(token) {
+    const pathname = `users/${token}`;
+    return url.format({
+        hostname: "https://code-eloquently-es6-blackjack-dapperauteur.c9users.io",
+        port: 8081,
+        pathname
+    });
+}
+
+export function fetchUser(token) {
+    return fetch(makeUrl(token), {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json());
+}
+
+// function in api utility file that sends a PATCH request to Rails server, similar to GET request to fetch record, only difference is this one needs to send some data, (new/loss,tie counters) to server using body with request
+export function patchUser(token, body) {
+    return fetch(makeUrl(token), {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }).then(response => response.json());
+}
